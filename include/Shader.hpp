058af000f89c5860b44c95f58cdaa30785b43abc
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2024-09-16 18:51:57 +0200

    Uniform location lookup improvement

--- include/Shader.hpp
+++ include/Shader.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include <unordered_map>
 #include <glm/glm.hpp>
 
 class Shader {
@@ -10,18 +11,21 @@ public:
     ~Shader();
     void load(const char* vertexPath, const char* fragmentPath);
     void use() const;
-    void setBool(const std::string& name, bool value) const;
-    void setInt(const std::string& name, int value) const;
-    void setFloat(const std::string& name, float value) const;
-    void setVec2(const std::string& name, const glm::vec2& value) const;
-    void setVec2(const std::string& name, float x, float y) const;
-    void setVec3(const std::string& name, const glm::vec3& value) const;
-    void setVec3(const std::string& name, float x, float y, float z) const;
-    void setVec4(const std::string& name, const glm::vec4& value) const;
-    void setVec4(const std::string& name, float x, float y, float z, float w) const;
-    void setMat2(const std::string& name, const glm::mat2& value) const;
-    void setMat3(const std::string& name, const glm::mat3& value) const;
-    void setMat4(const std::string& name, const glm::mat4& value) const;
+    void setBool(const std::string& name, bool value);
+    void setInt(const std::string& name, int value);
+    void setFloat(const std::string& name, float value);
+    void setVec2(const std::string& name, const glm::vec2& value);
+    void setVec2(const std::string& name, float x, float y);
+    void setVec3(const std::string& name, const glm::vec3& value);
+    void setVec3(const std::string& name, float x, float y, float z);
+    void setVec4(const std::string& name, const glm::vec4& value);
+    void setVec4(const std::string& name, float x, float y, float z, float w);
+    void setMat2(const std::string& name, const glm::mat2& value);
+    void setMat3(const std::string& name, const glm::mat3& value);
+    void setMat4(const std::string& name, const glm::mat4& value);
 private:
+    int32_t getUniformLocation(const std::string& name);
+
     uint32_t m_id;
+    std::unordered_map<std::string, int32_t> m_uniformLocationCache;
 };
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -81,50 +81,60 @@ void Shader::use() const {
     glUseProgram(m_id);
 }
 
-void Shader::setBool(const std::string& name, bool value) const {
-    glUniform1i(glGetUniformLocation(m_id, name.c_str()), (int)value);
+void Shader::setBool(const std::string& name, bool value) {
+    glUniform1i(getUniformLocation(name), (int)value);
 }
 
-void Shader::setInt(const std::string& name, int value) const {
-    glUniform1i(glGetUniformLocation(m_id, name.c_str()), value);
+void Shader::setInt(const std::string& name, int value) {
+    glUniform1i(getUniformLocation(name), value);
 }
 
-void Shader::setFloat(const std::string& name, float value) const {
-    glUniform1f(glGetUniformLocation(m_id, name.c_str()), value);
+void Shader::setFloat(const std::string& name, float value) {
+    glUniform1f(getUniformLocation(name), value);
 }
 
-void Shader::setVec2(const std::string& name, const glm::vec2& value) const {
-    glUniform2fv(glGetUniformLocation(m_id, name.c_str()), 1, &value[0]);
+void Shader::setVec2(const std::string& name, const glm::vec2& value) {
+    glUniform2fv(getUniformLocation(name), 1, &value[0]);
 }
 
-void Shader::setVec2(const std::string& name, float x, float y) const {
-    glUniform2f(glGetUniformLocation(m_id, name.c_str()), x, y);
+void Shader::setVec2(const std::string& name, float x, float y) {
+    glUniform2f(getUniformLocation(name), x, y);
 }
 
-void Shader::setVec3(const std::string& name, const glm::vec3& value) const {
-    glUniform3fv(glGetUniformLocation(m_id, name.c_str()), 1, &value[0]);
+void Shader::setVec3(const std::string& name, const glm::vec3& value) {
+    glUniform3fv(getUniformLocation(name), 1, &value[0]);
 }
 
-void Shader::setVec3(const std::string& name, float x, float y, float z) const {
-    glUniform3f(glGetUniformLocation(m_id, name.c_str()), x, y, z);
+void Shader::setVec3(const std::string& name, float x, float y, float z) {
+    glUniform3f(getUniformLocation(name), x, y, z);
 }
 
-void Shader::setVec4(const std::string& name, const glm::vec4& value) const {
-    glUniform4fv(glGetUniformLocation(m_id, name.c_str()), 1, &value[0]);
+void Shader::setVec4(const std::string& name, const glm::vec4& value) {
+    glUniform4fv(getUniformLocation(name), 1, &value[0]);
 }
 
-void Shader::setVec4(const std::string& name, float x, float y, float z, float w) const {
-    glUniform4f(glGetUniformLocation(m_id, name.c_str()), x, y, z, w);
+void Shader::setVec4(const std::string& name, float x, float y, float z, float w) {
+    glUniform4f(getUniformLocation(name), x, y, z, w);
 }
 
-void Shader::setMat2(const std::string& name, const glm::mat2& value) const {
-    glUniformMatrix2fv(glGetUniformLocation(m_id, name.c_str()), 1, GL_FALSE, &value[0][0]);
+void Shader::setMat2(const std::string& name, const glm::mat2& value) {
+    glUniformMatrix2fv(getUniformLocation(name), 1, GL_FALSE, &value[0][0]);
 }
 
-void Shader::setMat3(const std::string& name, const glm::mat3& value) const {
-    glUniformMatrix3fv(glGetUniformLocation(m_id, name.c_str()), 1, GL_FALSE, &value[0][0]);
+void Shader::setMat3(const std::string& name, const glm::mat3& value) {
+    glUniformMatrix3fv(getUniformLocation(name), 1, GL_FALSE, &value[0][0]);
 }
 
-void Shader::setMat4(const std::string& name, const glm::mat4& value) const {
-    glUniformMatrix4fv(glGetUniformLocation(m_id, name.c_str()), 1, GL_FALSE, &value[0][0]);
+void Shader::setMat4(const std::string& name, const glm::mat4& value) {
+    glUniformMatrix4fv(getUniformLocation(name), 1, GL_FALSE, &value[0][0]);
+}
+
+int32_t Shader::getUniformLocation(const std::string& name) {
+    auto locationIterator = m_uniformLocationCache.find(name);
+    if (locationIterator != m_uniformLocationCache.end()) {
+        return locationIterator->second;
+    }
+    int32_t location = glGetUniformLocation(m_id, name.c_str());
+    m_uniformLocationCache[name] = location;
+    return location;
 }
